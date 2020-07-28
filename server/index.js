3495333f404bd76ce46ca2f1b80bc9df1fc8cc7a
Nick Mallas <nmallas23@gmail.com>
2020-07-27 23:40:16 -0500

    completed up to 7b

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const { readFile } = require('fs').promises;
 const path = require('path');
+const { Item } = require("../models");
 
 const server = http.createServer(async(req, res) => {
     const url = req.url;
@@ -22,11 +23,40 @@ const server = http.createServer(async(req, res) => {
             res.setHeader('Content-Type', 'text/plain')
             res.end("Page Not Found")
         }
-        // console.log(imageFileContents);
-    } else {
+    } else if(url === "/items/new") {
+        let content = await readFile("./views/add-item.html");
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('I have items');
+        res.setHeader('Content-type', 'text/html');
+        res.write(content);
+        res.end();
+    } else if(url === "/items" && req.method === "POST") {
+        let body = '';
+        for await (let chunk of req) {
+            body += chunk;
+        }
+        let spaced = body.replace(/\+/g, ' ');
+        let decoded = decodeURIComponent(spaced);
+        let items = decoded.split('&');
+        let keyValues = items.map(item => item.split('='));
+        let obj = {createdAt: new Date(), updatedAt: new Date(), imageName: null}
+        for(let i=0; i<keyValues.length; i++) {
+            let [key, value] = keyValues[i];
+            obj[key] = value;
+        }
+        // const newItem = new Item();
+        // newItem.dataValues = obj;
+        // console.log(newItem);
+        // let insert = await Item.upsert('Items', obj);
+        res.setHeader('Content-type', 'text/html');
+        // res.write();
+        res.end();
+    }else {
+        let items = await Item.findAll();
+        let numItems = items.length;
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(`<div><a href="/items/new">Add a new item</a></div>
+        <h1>I have ${numItems} items</h1>`);
         return;
     }
 })
